5f52ee447e7abdde3463e94e6c64e45abdc603ba
Artem Golubev <agolubev.n@gmail.com>
2019-08-22 15:07:33 +0300

    added services

---
+++ docker/services/prometheus/prometheus_server/prometheus_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo -u prometheus /usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
